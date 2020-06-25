506809b033da782797d30e3f7b998648cf9313d9
Martin Page <wookoouk@gmail.com>
2020-06-25 16:06:29 +0100

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -3,13 +3,13 @@
 // upload file (waterpolo.bam) into /komondor with UUID name (123456.bam) and keep record of new name
 
 //VALIDATE study
-//ADD study
+//ADD study, keep track of accession
 
 //VALIDATE sample
-//ADD sample
+//ADD sample, keep track of accession
 
 //VALIDATE experiment
-//ADD experiment
+//ADD experiment, keep track of accession
 
 //VALIDATE run
-//ADD run
+//ADD run, keep track of accession
