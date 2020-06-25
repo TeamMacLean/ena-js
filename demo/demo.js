7d60b7a96ed33259512b2900ddd2c0e9c19460be
Martin Page <wookoouk@gmail.com>
2020-06-25 16:04:20 +0100

    Create demo.js

---
+++ demo/demo.js
@@ -0,0 +1,15 @@
+// connect to FTP
+
+// upload file into /komondor with UUID name (123456.fq.gz) and keep record of new name
+
+//VALIDATE study
+//ADD study
+
+//VALIDATE sample
+//ADD sample
+
+//VALIDATE experiment
+//ADD experiment
+
+//VALIDATE run
+//ADD run
